e6a3b5b241abc73d8ae7c6417382ba688e7d6d60
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2024-08-14 07:17:04 +0800

    feat: add random cat 2

--- src/handlers/random_handler.rs
+++ src/handlers/random_handler.rs
@@ -1,19 +1,41 @@
 use axum::response::IntoResponse;
 use reqwest::{header, Client, StatusCode};
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct CatResponse {
+    _id: String,
+}
 
 pub async fn get_random_cat() -> Result<impl IntoResponse, impl IntoResponse> {
     let client = Client::new();
-    let url = "https://cataas.com/cat";
+    let url = "https://cataas.com/cat?json=true";
 
-    let res = client.get(url).send().await.unwrap();
+    let result = client.get(url).send().await;
+    let data = result.unwrap().json::<CatResponse>().await.unwrap();
 
-    match res.status() {
-        StatusCode::OK => Ok((
-            StatusCode::OK,
-            [(header::CONTENT_TYPE, "image/jpeg")],
-            res.bytes().await.unwrap(),
-        )),
-        _ => Err((
+    let result = client
+        .get("https://cataas.com/cat/".to_string() + &data._id)
+        .send()
+        .await;
+
+    match result {
+        Ok(res) => {
+            if res.status() == StatusCode::OK {
+                Ok((
+                    StatusCode::OK,
+                    [(header::CONTENT_TYPE, "image/jpeg")],
+                    res.bytes().await.unwrap(),
+                ))
+            } else {
+                Err((
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    "Failed to fetch random cat image",
+                )
+                    .into_response())
+            }
+        }
+        Err(_) => Err((
             StatusCode::INTERNAL_SERVER_ERROR,
             "Failed to fetch random cat image",
         )
