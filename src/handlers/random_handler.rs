58a7a90188a10ee105f9da1fb2b6bf1574a32c6d
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2024-08-14 07:04:57 +0800

    feat: add random cat

--- src/handlers/random_handler.rs
+++ src/handlers/random_handler.rs
@@ -1,10 +1,22 @@
-use axum::http::Response;
+use axum::response::IntoResponse;
+use reqwest::{header, Client, StatusCode};
 
-pub fn get_random_cat() -> Response<String> {
-    let response_builder = Response::builder();
+pub async fn get_random_cat() -> Result<impl IntoResponse, impl IntoResponse> {
+    let client = Client::new();
+    let url = "https://cataas.com/cat";
 
-    response_builder
-        .status(200)
-        .body("Random cat".into())
-        .unwrap()
+    let res = client.get(url).send().await.unwrap();
+
+    match res.status() {
+        StatusCode::OK => Ok((
+            StatusCode::OK,
+            [(header::CONTENT_TYPE, "image/jpeg")],
+            res.bytes().await.unwrap(),
+        )),
+        _ => Err((
+            StatusCode::INTERNAL_SERVER_ERROR,
+            "Failed to fetch random cat image",
+        )
+            .into_response()),
+    }
 }
--- src/routes/random_route.rs
+++ src/routes/random_route.rs
@@ -1,12 +1,9 @@
-use axum::Router;
+use axum::{routing::get, Router};
 
 use crate::handlers::random_handler;
 
 pub fn routes() -> Router {
-    let router = Router::new().route(
-        "/cats",
-        axum::routing::get(random_handler::get_random_cat()),
-    );
+    let router = Router::new().route("/cats", get(random_handler::get_random_cat));
 
     return router;
 }
