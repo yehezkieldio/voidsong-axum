201520fae113ae53130e4d2d0bb07dec5bf860a8
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2024-08-14 06:20:00 +0800

    feat: use response builder

--- src/handlers/random_handler.rs
+++ src/handlers/random_handler.rs
@@ -1,3 +1,10 @@
-pub fn get_random_cat() -> String {
-    "Cat".to_string()
+use axum::http::Response;
+
+pub fn get_random_cat() -> Response<String> {
+    let response_builder = Response::builder();
+
+    response_builder
+        .status(200)
+        .body("Random cat".into())
+        .unwrap()
 }
