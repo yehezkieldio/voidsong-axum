4e2f4e84f00e04924594f453338c27a5ba52aa22
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2024-08-15 07:08:20 +0800

    feat: add random cat aggregator

--- src/handlers/random_handler.rs
+++ src/handlers/random_handler.rs
@@ -9,8 +9,57 @@ use crate::utils::{
     url::{fetch_image, preflight_check},
 };
 
+// vec!["https://cataas.com/cat?json=true", "https://api.thecatapi.com/v1/images/search"];
+
 #[derive(Deserialize)]
-struct DogResponse {
+struct CatAsService {
+    _id: String,
+}
+
+#[derive(Deserialize)]
+struct TheCatAPI {
+    url: String,
+}
+
+pub async fn get_random_cat(State(state): State<AppState>) -> Result<VoidsongImage, VoidsongError> {
+    let urls: Vec<&str> = vec![
+        "https://cataas.com/cat?json=true",
+        "https://api.thecatapi.com/v1/images/search",
+    ];
+    let client: MutexGuard<Client> = state.client.lock().await;
+
+    // Check if the APIs are available
+    let (success, url) = preflight_check(&client, urls).await;
+    if !success {
+        return Err(VoidsongError::ServiceUnavailable);
+    }
+
+    // Get the image URL
+    let get_url: Response = match client.get(url.unwrap()).headers(user_agent()).send().await {
+        Ok(response) => response,
+        Err(_) => return Err(VoidsongError::FailedToFetchImage),
+    };
+
+    let url: String = match url.unwrap() {
+        "https://cataas.com/cat?json=true" => match get_url.json::<CatAsService>().await {
+            Ok(response) => format!("https://cataas.com/cat/{}", response._id),
+            Err(_) => return Err(VoidsongError::FailedToFetchImage),
+        },
+        "https://api.thecatapi.com/v1/images/search" => {
+            match get_url.json::<Vec<TheCatAPI>>().await {
+                Ok(response) => response[0].url.clone(),
+                Err(_) => return Err(VoidsongError::FailedToFetchImage),
+            }
+        }
+        _ => return Err(VoidsongError::FailedToFetchImage),
+    };
+
+    let image: Option<VoidsongImage> = fetch_image(client, url.as_str()).await;
+    image.ok_or(VoidsongError::FailedToFetchImage)
+}
+
+#[derive(Deserialize)]
+struct DogCEO {
     message: String,
 }
 
@@ -30,7 +79,7 @@ pub async fn get_random_dog(State(state): State<AppState>) -> Result<VoidsongIma
         Err(_) => return Err(VoidsongError::FailedToFetchImage),
     };
 
-    let url: String = match get_url.json::<DogResponse>().await {
+    let url: String = match get_url.json::<DogCEO>().await {
         Ok(response) => response.message,
         Err(_) => return Err(VoidsongError::FailedToFetchImage),
     };
--- src/routes/random_route.rs
+++ src/routes/random_route.rs
@@ -8,8 +8,10 @@ pub fn routes() -> Router {
     let state: AppState = AppState::new();
 
     let animals: Router = Router::new()
+        .route("/cat", get(random_handler::get_random_cat))
         .route("/dog", get(random_handler::get_random_dog))
         .with_state(state);
+
     let router: Router = Router::new().nest("/animal", animals);
 
     router
