6d12adbd801f47e415c34b82f11a26ed8ed2a000
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2024-08-14 22:56:26 +0800

    refactor: use default values if env file not found

--- src/env.rs
+++ src/env.rs
@@ -6,7 +6,10 @@ const SERVER_HOST: &str = "0.0.0.0";
 const SERVER_PORT: &str = "8080";
 
 pub fn load() -> (String, String) {
-    dotenv().expect("Failed to load .env file");
+    // load .env file if it exists if not use default values
+    if dotenv().is_err() {
+        return (SERVER_HOST.to_string(), SERVER_PORT.to_string());
+    }
 
     let host: String = env::var("SERVER_HOST").unwrap_or_else(|_| SERVER_HOST.to_string());
     let port: String = env::var("SERVER_PORT").unwrap_or_else(|_| SERVER_PORT.to_string());
