5a73ccf275ddbf197fa5367fa34c8fc3e83956ef
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2024-08-17 06:11:53 +0800

    feat: add compression

--- src/routes/root.rs
+++ src/routes/root.rs
@@ -1,5 +1,6 @@
 use axum::Router;
 use tower_http::{
+    compression::CompressionLayer,
     normalize_path::{NormalizePath, NormalizePathLayer},
     trace::{self, TraceLayer},
 };
@@ -9,11 +10,14 @@ use tracing::Level;
 use super::random_route;
 
 pub fn routes() -> NormalizePath<Router> {
-    let app_router = Router::new().nest("/random", random_route::routes()).layer(
-        TraceLayer::new_for_http()
-            .make_span_with(trace::DefaultMakeSpan::new().level(Level::INFO))
-            .on_response(trace::DefaultOnResponse::new().level(Level::INFO)),
-    );
+    let app_router = Router::new()
+        .nest("/random", random_route::routes())
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(trace::DefaultMakeSpan::new().level(Level::INFO))
+                .on_response(trace::DefaultOnResponse::new().level(Level::INFO)),
+        )
+        .layer(CompressionLayer::new());
 
     NormalizePathLayer::trim_trailing_slash().layer(app_router)
 }
